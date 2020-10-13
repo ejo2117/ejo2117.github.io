050593d6e82c7a0d49d46a0f470b0ebded0643fe
ejo2117 <ejo2117@columbia.edu>
2020-10-13 01:21:40 -0400

    update cube smh

--- js/hypercube.js
+++ js/hypercube.js
@@ -104,7 +104,7 @@ function init() {
         geometry.vertices.push(vertexCoords[vertexJoins[i][1]]);
         let line = new THREE.Line(geometry, new THREE.LineBasicMaterial({
             color: 0x22A7F0,
-            linewidth: 4
+            linewidth: 2
         }));
         scene.add(line);
         edges[i] = line;
