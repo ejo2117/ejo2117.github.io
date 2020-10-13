dc2032d37bf2ed6ae8de2bf0be0dbf58f61c0f37
ejo2117 <ejo2117@columbia.edu>
2020-10-12 20:55:23 -0400

    updated visualizer code
    updated visualizer code

--- js/main.js
+++ js/main.js
@@ -30,43 +30,59 @@ function shiftColors(doodle) {
 }
 
 
-//set up audio context
+// Set up audio context
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioContext = new AudioContext();
-let currentBuffer = null;
 
-const visualizeAudio = url => {
+/**
+ * Retrieves audio from an external source, the initializes the drawing function
+ * @param {String} url the url of the audio we'd like to fetch
+ */
+const drawAudio = url => {
     fetch(url)
         .then(response => response.arrayBuffer())
         .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
-        .then(audioBuffer => visualizeAudio(audioBuffer));
-}
+        .then(audioBuffer => draw(normalizeData(filterData(audioBuffer))));
+};
 
-const filerData = audioBuffer => {
-    const rawData = audioBuffer.getChannelData(0);
-    const samples = 70;
-    const blockSize = Math.floor(rawData.length / samples);
+/**
+ * Filters the AudioBuffer retrieved from an external source
+ * @param {AudioBuffer} audioBuffer the AudioBuffer from drawAudio()
+ * @returns {Array} an array of floating point numbers
+ */
+const filterData = audioBuffer => {
+    const rawData = audioBuffer.getChannelData(0); // We only need to work with one channel of data
+    const samples = 70; // Number of samples we want to have in our final data set
+    const blockSize = Math.floor(rawData.length / samples); // the number of samples in each subdivision
     const filteredData = [];
     for (let i = 0; i < samples; i++) {
-        let blockStart = blockSize * i;
+        let blockStart = blockSize * i; // the location of the first sample in the block
         let sum = 0;
         for (let j = 0; j < blockSize; j++) {
-            sum = sum + Math.abs(rawData[blockStart + j])
+            sum = sum + Math.abs(rawData[blockStart + j]); // find the sum of all the samples in the block
         }
-        filteredData.push(sum / blockSize)
-
+        filteredData.push(sum / blockSize); // divide the sum by the block size to get the average
     }
     return filteredData;
+};
 
-}
+/**
+ * Normalizes the audio data to make a cleaner illustration 
+ * @param {Array} filteredData the data from filterData()
+ * @returns {Array} an normalized array of floating point numbers
+ */
 const normalizeData = filteredData => {
     const multiplier = Math.pow(Math.max(...filteredData), -1);
     return filteredData.map(n => n * multiplier);
-
 }
 
-const draw = normalizeData => {
-    //set up canvas
+/**
+ * Draws the audio file into a canvas element.
+ * @param {Array} normalizedData The filtered array returned from filterData()
+ * @returns {Array} a normalized array of data
+ */
+const draw = normalizedData => {
+    // set up the canvas
     const canvas = document.querySelector("#waveform");
     const dpr = window.devicePixelRatio || 1;
     const padding = 20;
@@ -74,10 +90,10 @@ const draw = normalizeData => {
     canvas.height = (canvas.offsetHeight + padding * 2) * dpr;
     const ctx = canvas.getContext("2d");
     ctx.scale(dpr, dpr);
-    ctx.translate(0, canvas.offsetHeight / 2 + padding);
+    ctx.translate(0, canvas.offsetHeight / 2 + padding); // set Y = 0 to be in the middle of the canvas
 
-    //draw the line segments
-    const width = canvas.offsetWidth / normalizedData / length;
+    // draw the line segments
+    const width = canvas.offsetWidth / normalizedData.length;
     for (let i = 0; i < normalizedData.length; i++) {
         const x = width * i;
         let height = normalizedData[i] * canvas.offsetHeight - padding;
@@ -88,19 +104,29 @@ const draw = normalizeData => {
         }
         drawLineSegment(ctx, x, height, width, (i + 1) % 2);
     }
-}
+};
 
-const drawLineSegment = (ctx, x, y, width, isEven) => {
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "#fff";
+/**
+ * A utility function for drawing our line segments
+ * @param {AudioContext} ctx the audio context 
+ * @param {number} x  the x coordinate of the beginning of the line segment
+ * @param {number} height the desired height of the line segment
+ * @param {number} width the desired width of the line segment
+ * @param {boolean} isEven whether or not the segmented is even-numbered
+ */
+const drawLineSegment = (ctx, x, height, width, isEven) => {
+    ctx.lineWidth = 1; // how thick the line is
+    ctx.strokeStyle = "#fff"; // what color our line is
     ctx.beginPath();
-    y = isEven ? y : -y;
+    height = isEven ? height : -height;
     ctx.moveTo(x, 0);
-    ctx.lineTo(x, y);
-    ctx.arc(x = width / 2, y, wdth / 2, Math.PI, Math.PI, isEven);
+    ctx.lineTo(x, height);
+    ctx.arc(x + width / 2, height, width / 2, Math.PI, 0, isEven);
     ctx.lineTo(x + width, 0);
     ctx.stroke();
-}
+};
+
+
 $(() => {
     //hover color shift
     $('.title').each((i, e) => {
@@ -162,9 +188,7 @@ $(() => {
             $('.content.' + content).addClass('showing');
             if (content == "player") {
                 console.log('start visualizer')
-                let response = await visualizeAudio('src/allubaby.wav');
-                console.log(response)
-
+                drawAudio('src/allubaby.wav');
             }
             //sound.play();
             // wavesurfer.load('src/allubaby.wav')
